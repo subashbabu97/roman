15acfa0c417450337401353d96651e1ecc9dab60
subashbabu97 <48273911+subashbabu97@users.noreply.github.com>
2019-06-07 12:45:20 +0530

    Update number.py

--- number.py
+++ number.py
@@ -1,3 +1,4 @@
 a=['0','I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
-b=int(input())
-print(a[b])
+b_str=(input())
+if b in a:
+        print(a.index[b])
