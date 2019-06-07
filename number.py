b5b2396c4f5605ffb211422767c945bd4d3c806b
subashbabu97 <48273911+subashbabu97@users.noreply.github.com>
2019-06-07 13:47:15 +0530

    Update number.py

--- number.py
+++ number.py
@@ -1,4 +1,4 @@
 a_str=['0','I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
 b_str=(input())
 if b_str in a_str:
-        print(a.index[b_str])
+        print(a_str.index[b_str])
